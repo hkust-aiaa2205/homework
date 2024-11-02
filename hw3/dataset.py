90ebdafcca3c3f9d2cb39e93de1c157906035d47
Iris <rli335@connect.hkust-gz.edu.cn>
2024-11-02 12:53:32 +0800

    Update dataset.py

--- hw3/dataset.py
+++ hw3/dataset.py
@@ -8,25 +8,29 @@ from sklearn.model_selection import train_test_split
 import random
 
 class MyDataset(Dataset):
-    def __init__(self, root, csv_file,stage="train",ratio=0.2, transform=None):
+    def __init__(self, root, csv_file, stage="train", ratio=0.2, transform=None):
         self.root = root
         self.transforms = transform
         self.df = pd.read_csv(csv_file, header=None, skiprows=1)
         self.stage = stage
         self.ratio = ratio
-        self.files = None
-        filenames = self.df[0].unique()
-        if self.stage == "test":
-            self.files = filenames
+        self.files = self._get_files()
+
+    def _get_files(self):
+        filenames = self.df[0].tolist()
+        length = len(filenames)
+        print(f'all length is {length}')
+        train_files = filenames[int(length * self.ratio):]
+        val_files = filenames[:int(length * self.ratio)]
+        print(f'len train is {len(train_files)}, len val is {len(val_files)}')
+        if self.stage == "train":
+            return train_files
+        elif self.stage == "val":
+            return val_files
+        elif self.stage == "test":
+            return filenames
         else:
-            filenames = sorted(filenames)
-            length = len(filenames)
-            train_files = filenames[int(length * self.ratio):]
-            val_files = filenames[:int(length * self.ratio)]
-            if self.stage == "train":
-                self.files = train_files
-            elif self.stage == "val":
-                self.files = val_files
+            raise ValueError("stage should be either 'train', 'val' or 'test'")
 
     def __len__(self):
         return len(self.files)
@@ -37,13 +41,6 @@ class MyDataset(Dataset):
         label = self.df.loc[self.df[0] == vid, 1].values[0]
         img_list = os.listdir(os.path.join(self.root, f"{vid}.mp4"))
         img_list = sorted(img_list)
-        img_16fpv = []    
-        for i in range(len(img_list)):
-            img_path = os.path.join(self.root, f"{vid}.mp4", img_list[i])
-            img = Image.open(img_path).convert('RGB')
-            if self.transforms is not None:
-                img = self.transforms(img)
-            img_16fpv.append(img)
+        img_16fpv = [self.transforms(Image.open(os.path.join(self.root, f"{vid}.mp4", img_path)).convert('RGB')) for img_path in img_list]
         img_16fpv_tensor = torch.stack(img_16fpv).permute(1,0,2,3)
         return img_16fpv_tensor, label
-

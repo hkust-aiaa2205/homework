4fbe638b88373f2f391f1ebdd48a5afc2d3df27f
Iris <rli335@connect.hkust-gz.edu.cn>
2024-11-02 12:58:13 +0800

    Update models.py

--- hw3/models.py
+++ hw3/models.py
@@ -5,17 +5,16 @@ import torchvision.models as models
 class VideoResNet(nn.Module):
     def __init__(self, num_classes):
         super(VideoResNet, self).__init__()
-        # 加载预训练的 ResNet-3D 模型
+        # Load pre-trained ResNet-3D models
         self.r3d_18 = models.video.r3d_18(pretrained=True)
         
-        # 冻结除了最后一个全连接层以外的所有参数
+        # Freeze all parameters except the last fully connected layer
         for name, param in self.r3d_18.named_parameters():
             if 'fc' not in name:
                 param.requires_grad = False
         
-        # 替换最后的全连接层以适应新的分类任务
+        # Replace the last fully connected layer to accommodate the new classification task
         self.r3d_18.fc = nn.Linear(self.r3d_18.fc.in_features, num_classes)
     
     def forward(self, x):
-        # 前向传播
         return self.r3d_18(x)

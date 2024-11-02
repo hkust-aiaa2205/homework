7d6530093dc348ca1f86a7df78c2265fe426a3c2
Iris <rli335@connect.hkust-gz.edu.cn>
2024-11-02 13:00:34 +0800

    Update test2csv.py

--- hw3/test2csv.py
+++ hw3/test2csv.py
@@ -1,42 +1,39 @@
 import os
-# os.environ['CUDA_VISIBLE_DEVICES'] = '1'
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import pandas as pd
-from torch.utils.data import Dataset, DataLoader
-import torchvision.transforms as transforms
 from PIL import Image
 from dataset import MyDataset
 from models import VideoResNet
 from tqdm import tqdm
 
-# 检查cuda是否可用
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.utils.data import Dataset, DataLoader
+import torchvision.transforms as transforms
 
-# 定义数据预处理
+# Define aug
 transforms = transforms.Compose([
     transforms.Resize((224, 224)),
     transforms.ToTensor(),
-    # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) # 归一化操作
+    # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) # Normalization
 ])
 
-# 加载测试数据集
+# Load test dataset
 test_dataset = MyDataset("/root/hkustgz-aiaa-5032-hw3/hw3_16fpv", 
 "/root/hw-private-main/hw3/test.csv", 
 stage="test", ratio=0.2, transform=transforms)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 print(f"Length of test loader: {len(test_loader)}")
 
-# 加载模型
+# Load model
 model = VideoResNet(num_classes=10).cuda()
 model.load_state_dict(torch.load('/root/hw-private-main/hw3/ResNet18_last.pth'))
 
-# 加载视频ID
+# Load video ID
 fread = open("/root/hw-private-main/hw3/hkustgz-aiaa-5032-hw3/test_for_student.label", "r")
 video_ids = [os.path.splitext(line.strip())[0] for line in fread.readlines()]
 
-# 测试模型
+# Val stage
 model.eval()
 result = []
 with torch.no_grad():
@@ -47,7 +44,7 @@ with torch.no_grad():
         _, predicted = torch.max(outputs.data, 1)
         result.extend(predicted.cpu().numpy())
 
-# 保存结果
+# Save result
 with open('result_ResNet18_3D.csv', "w") as f:
     f.writelines("Id,Category\n")
     for i, pred_class in enumerate(result):

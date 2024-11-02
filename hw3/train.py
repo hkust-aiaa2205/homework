fba6d5bbc8e5ee1cf84d918184020134bffb7ad0
Iris <rli335@connect.hkust-gz.edu.cn>
2024-11-02 12:53:01 +0800

    Update train.py

--- hw3/train.py
+++ hw3/train.py
@@ -4,38 +4,40 @@ import torch.nn.functional as F
 import pandas as pd
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
-import os
 from PIL import Image
 from sklearn.model_selection import train_test_split
 import time
 from dataset import MyDataset
-from models import resnet18
+from models import VideoResNet
+import os
 
-device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-
+# os.environ['CUDA_VISIBLE_DEVICES'] = '7'
+torch.backends.cudnn.benchmark = True
 
 transforms = transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) 
-        ])
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    # transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)) 
+])
 
+val_dataset = MyDataset('/root/hkustgz-aiaa-5032-hw3/hw3_16fpv', '/root/hkustgz-aiaa-5032-hw3/trainval.csv', stage="val", ratio=0.2, transform=transforms)
+train_dataset = MyDataset('/root/hkustgz-aiaa-5032-hw3/hw3_16fpv', '/root/hkustgz-aiaa-5032-hw3/trainval.csv', stage="train", ratio=0.2, transform=transforms)
 
-train_dataset = MyDataset("/hw3_16fpv", "trainval.csv", stage="train",ratio=0.2,transform=transforms)
-val_dataset = MyDataset("/hw3_16fpv", "trainval.csv", stage="val",ratio=0.2,transform=transforms)
 print('dataset loaded')
-train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
-print(len(train_loader))
-print(len(val_loader))
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=2, pin_memory=True, drop_last=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False, num_workers=2, pin_memory=True)
 
-net = resnet18(num_classes=10, sample_size=224, sample_duration=16).to(device)
-optimizer = torch.optim.SGD(net.parameters(), lr=0.001, momentum=0.9, weight_decay=0.01)
+print('train ', len(train_loader))
+print('val ', len(val_loader))
+
+model = VideoResNet(num_classes=10).cuda()
+optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 criterion = nn.CrossEntropyLoss()
 
+print("model loaded")
 
-patience = 5
-best_acc = 0
+best_acc_train = 0
+best_acc_val = 0
 counter = 0  # Initialize counter to track epochs since last improvement
 
 print('start training')
@@ -47,12 +49,13 @@ for epoch in range(50):
     total_train = 0
     correct_val = 0
     total_val = 0
-    
+
+    model.train()  # Set the model to train mode
+    print('train ...')
     for i, (inputs, labels) in enumerate(train_loader, 0):
-        # Training code ...
-        inputs, labels = inputs.to(device), labels.to(device)
+        inputs, labels = inputs.cuda(), labels.cuda()
         optimizer.zero_grad()
-        outputs = net(inputs)
+        outputs = model(inputs)
         loss_train = criterion(outputs, labels)
         loss_train.backward()
         optimizer.step()
@@ -60,30 +63,37 @@ for epoch in range(50):
         _, predicted_train = torch.max(outputs.data, 1)
         total_train += labels.size(0)
         correct_train += (predicted_train == labels).sum().item()
+
+    model.eval()  # Set the model to evaluation mode
+
     with torch.no_grad():
+        print('val ...')
         for val_inputs, val_labels in val_loader:
-            # Validation code ...
-            val_inputs, val_labels = val_inputs.to(device), val_labels.to(device)
-            val_outputs = net(val_inputs)
+            val_inputs, val_labels = val_inputs.cuda(), val_labels.cuda()
+            val_outputs = model(val_inputs)
             loss_val = criterion(val_outputs, val_labels)
             running_loss_val += loss_val.item()
             _, predicted_val = torch.max(val_outputs.data, 1)
             total_val += val_labels.size(0)
             correct_val += (predicted_val == val_labels).sum().item()
+    
+    acc_train = correct_train / total_train
+    acc_val = correct_val / total_val
+    
+    print(f'acc train   {acc_train}       {correct_train}/{total_train}')
+    print(f'acc val     {acc_val}         {correct_val}/{total_val}')
 
-    acc_val = correct_val/len(val_loader)
-    if acc_val > best_acc:
-        best_acc = acc_val
-        counter = 0  # Reset counter
-        torch.save(net.state_dict(), 'ResNet18_best.pth')
-    else:
-        counter += 1
-        if counter >= patience:
-            print('Early stopping: validation accuracy did not improve for {} epochs'.format(patience))
-            break  # Stop training loop
+    if acc_train > best_acc_train:
+        best_acc_train = acc_train
+        torch.save(model.state_dict(), './ResNet18_best_train.pth')
 
-    torch.save(net.state_dict(), 'ResNet18_last.pth')
-    end_time = time.time() 
-    print('[Epoch %d] Loss (train/val): %.3f/%.3f' % (epoch + 1, running_loss_train/len(train_loader), running_loss_val/len(val_loader)),
-          ' Acc (train/val): %.2f%%/%.2f%%' % (100 * correct_train/total_train, 100 * correct_val/total_val)
-          ,' Epoch Time: %.2f' % (end_time - start_time))
+    if acc_val > best_acc_val:
+        best_acc_val = acc_val
+        torch.save(model.state_dict(), './ResNet18_best_val.pth')
+  
+    torch.save(model.state_dict(), './ResNet18_last.pth')
+
+    end_time = time.time()  # 记录结束时间
+    print('[Epoch %d] Loss (train/val): %.3f/%.3f' % (epoch + 1, running_loss_train / len(train_loader), running_loss_val / len(val_loader)),
+          ' Acc (train/val): %.2f%%/%.2f%%' % (100 * correct_train / total_train, 100 * correct_val / total_val),
+          ' Epoch Time: %.2f' % (end_time - start_time))
